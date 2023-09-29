39a8221e796d864571462b5552f142587ba7d1c5
ccz <2047638475@qq.com>
2023-09-29 09:07:43 +0800

    feat: 🎸 添加readme

--- include/log/log.h
+++ include/log/log.h
@@ -1,8 +1,21 @@
 #pragma once
+#include "blockQueue.h"
 #include <iostream>
 #include <pthread.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <string>
 
-#include "blockQueue.h"
+using namespace std;
+
+class Log {
+private:
+    Log(/* args */);
+    ~Log();
+
+public:
+    static Log* get_instance() {
+        static Log instance;
+        return &instance;
+    }
+};
---
+++ readme.md
@@ -0,0 +1,4 @@
+# 高性能 web 服务器项目
+
+参考项目地址：
+https://github.com/qinguoyi/TinyWebServer
